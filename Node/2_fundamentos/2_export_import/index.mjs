a4bf43f0ead052e441cde3ae807b9f917e4670ff
Unknown Cheater <unknowncheater_@outlook.com>
2024-11-13 00:18:32 -0300

    atualizando códigos

--- Node/2_fundamentos/2_export_import/index.mjs
+++ Node/2_fundamentos/2_export_import/index.mjs
@@ -2,8 +2,8 @@
 // A extensão '.mjs' indica que este arquivo é um módulo JavaScript ES6, que utiliza a sintaxe moderna de importação/exportação.
 import soma from './meu_modulo.mjs'
 
-// Chama a função 'soma' com os argumentos 2 e 3, que somam esses valores.
+// Chamando a função 'soma' com os parâmetros 2 e 3, que somam esses valores.
 soma(2, 3)
 
-// Chama a função 'soma' novamente, agora com os argumentos 7 e 4.
+// Chamando a função 'soma' com os parâmetros 7 e 4
 soma(7, 4)
